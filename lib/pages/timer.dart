c9c6bbd148779c4f02bf9a9d44c0355a03450d5e
BAR-KEY <park96h@naver.com>
2022-02-16 01:20:39 +0900

    timer 를 만들어보자

---
+++ lib/pages/timer.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Timer extends StatefulWidget {
+  const Timer({Key? key}) : super(key: key);
+
+  @override
+  _TimerState createState() => _TimerState();
+}
+
+class _TimerState extends State<Timer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text('Timer'),
+    ));
+  }
+}
